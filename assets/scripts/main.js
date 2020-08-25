622424471f77ecdfae466b8467b621025179af70
Senadin <senadin.botic@gmail.com>
2020-08-26 00:07:21 +0200

    Initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+    // Back To Top animation
+    var $backToTop = $(".back-to-top");
+    $backToTop.hide();
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            $backToTop.fadeIn();
+        } else {
+            $backToTop.fadeOut();
+        }
+    });
+
+    $backToTop.on('click', function(e) {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+    });
+
+    // Adding Class on Scroll
+    $(window).on('load', function() {
+        var winOffset = document.documentElement.scrollTop || document.body.scrollTop;
+        if (winOffset > 1) {
+            $('body').addClass('nav-fixed');
+        }
+    });
+    $(window).scroll(function() {
+        var winOffset = document.documentElement.scrollTop || document.body.scrollTop;
+        if (winOffset > 1) {
+            $('body').addClass('nav-fixed');
+        } else {
+            $('body').removeClass('nav-fixed');
+        }
+    });
+
+    // Menu
+    $('.nav-icon-container').on('click', function() {
+        $('body').addClass('menu-open');
+    });
+    $('.close-icon-container').on('click', function() {
+        $('body').removeClass('menu-open');
+    });
+
+    // // Cases animation
+    // AOS.init({
+    //     duration: 1000
+    // });
+
+    // $(".jarallax").each(function (e, n) {
+    //     var speed = $(n).data("speed") || 0.85;
+    //     $(n).jarallax({
+    //         speed: speed,
+    //         automaticResize: true,
+    //         onInit: function () {
+    //             $(n).show();
+    //         }
+    //     });
+    // });
+
+    // // Custom Category Filter
+    // if (!(/Trident\/|MSIE/.test(window.navigator.userAgent)) && $('.filter').length) {
+    //     var filterSingle = $('.filter').filterizr({
+    //         setupControls: false,
+    //         animationDuration: 0.3
+    //     });
+    //     $('.filter-nav li').click(function () {
+    //         $('.filter-nav li').removeClass('active');
+    //         $(this).addClass('active');
+    //         var filter = $(this).data('fltr');
+    //     });
+    // }
+
+});
