6ea3d2a32e5da0b6a491c468ff6764188e548a3e
Senadin <senadin.botic@gmail.com>
2020-09-05 18:27:45 +0200

    Added new templates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -120,10 +120,32 @@ $(document).ready(function() {
         });
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+    if ($('#googleMap').length) {
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
 
     // Light Gallery
     $('.lightgallery').lightGallery({
         pager: false,
     });
+
+    // Form Validation
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('contact-form');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+
+    })();
 });
